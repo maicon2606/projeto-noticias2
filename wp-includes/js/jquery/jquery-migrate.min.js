49f84995c60dfb944b4603602ee3fa16e2e27a77
maicon gabriel <107694386+maicon2606@users.noreply.github.com>
2023-03-17 00:30:36 -0300

    Initial commit

---
+++ wp-includes/js/jquery/jquery-migrate.min.js
@@ -0,0 +1,74 @@
+@font-face {
+    font-family: "icons";
+    src: url("./icons.eot?91667668f57be64070c6c0722b95e502?#iefix") format("embedded-opentype"),
+url("./icons.woff2?91667668f57be64070c6c0722b95e502") format("woff2"),
+url("./icons.woff?91667668f57be64070c6c0722b95e502") format("woff"),
+url("./icons.ttf?91667668f57be64070c6c0722b95e502") format("truetype"),
+url("./icons.svg?91667668f57be64070c6c0722b95e502#icons") format("svg");
+}
+
+i[class^="icon-"]:before, i[class*=" icon-"]:before {
+    font-family: icons !important;
+    font-style: normal;
+    font-weight: normal !important;
+    font-variant: normal;
+    text-transform: none;
+    line-height: 1;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+
+.icon-calendario:before {
+    content: "\f101";
+}
+.icon-cartas-de-servicos:before {
+    content: "\f102";
+}
+.icon-email:before {
+    content: "\f103";
+}
+.icon-e-ouv:before {
+    content: "\f104";
+}
+.icon-facebook:before {
+    content: "\f105";
+}
+.icon-governo:before {
+    content: "\f106";
+}
+.icon-imagens:before {
+    content: "\f107";
+}
+.icon-information:before {
+    content: "\f108";
+}
+.icon-instagram:before {
+    content: "\f109";
+}
+.icon-lupa:before {
+    content: "\f10a";
+}
+.icon-play:before {
+    content: "\f10b";
+}
+.icon-printer:before {
+    content: "\f10c";
+}
+.icon-radio:before {
+    content: "\f10d";
+}
+.icon-servicos:before {
+    content: "\f10e";
+}
+.icon-transparencia:before {
+    content: "\f10f";
+}
+.icon-twitter:before {
+    content: "\f110";
+}
+.icon-user:before {
+    content: "\f111";
+}
+.icon-youtube:before {
+    content: "\f112";
+}
